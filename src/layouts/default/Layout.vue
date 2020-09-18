5a466b2d48b3f17ca5a7c4c94df10890a314e052
karahman <jakarahman007@gmail.com>
2020-09-18 21:21:42 +0700

    add admin & default layouts prototype

---
+++ src/layouts/default/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Default Layout</h1>
+
+    <!-- Slot -->
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+        
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
