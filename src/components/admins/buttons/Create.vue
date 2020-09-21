5145a853c3fe07657dc5e809d7059f993e84ff0c
karahman <jakarahman007@gmail.com>
2020-09-21 12:50:53 +0700

    add create button

---
+++ src/components/admins/buttons/Create.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn
+    dark
+    color="createButton"
+  >
+    <v-icon>{{ icon }}</v-icon>
+
+    <span>{{ text }}</span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: 'mdi mdi-plus',
+    },
+  },
+}
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify, { colors } from 'vuetify/lib'
 
 Vue.use(Vuetify)
 
+const buttons = {
+  create: colors.green.lighten1,
+}
+
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        createButton: buttons.create,
+      },
+      dark:{
+        createButton: buttons.create,
+      },
+    },
+  },
 })
