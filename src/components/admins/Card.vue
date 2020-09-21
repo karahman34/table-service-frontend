6664575ec000589edd5bb825f793a1044dfbbd40
karahman <jakarahman007@gmail.com>
2020-09-21 21:51:34 +0700

    add Card & Search components

---
+++ src/components/admins/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card
+    :elevation="elevation"
+    :style="{'border-radius': borderRadius}"
+  >
+    <slot />
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    elevation: {
+      type: [String, Number],
+      default: 4, 
+    },
+
+    borderRadius: {
+      type: String,
+      default: '5px',
+    },
+  },
+}
+</script>
---
+++ src/components/admins/Search.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-text-field
+    v-model="search"
+    single-line
+    hide-details
+    placeholder="Search..."
+    prepend-inner-icon="mdi mdi-magnify"
+    class="mb-2"
+    :loading="loading"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      search: null,
+      timeout: null,
+    }
+  },
+
+  watch: {
+    search(val) {
+      clearTimeout(this.timeout)
+
+      this.timeout = setTimeout(() => this.$emit('search', val), 600)
+    },
+  },
+}
+</script>
