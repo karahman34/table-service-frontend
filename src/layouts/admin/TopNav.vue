bb3bc7f0afc1ba8a93c4d97c20193f471f6d38fe
karahman <jakarahman007@gmail.com>
2020-10-04 16:20:16 +0700

    add icon to username & change logo to router

--- src/layouts/admin/TopNav.vue
+++ src/layouts/admin/TopNav.vue
@@ -7,12 +7,21 @@
     elevation="6"
   >
     <v-app-bar-nav-icon @click="$emit('toggle-sidebar')" />
-    <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+
+    <v-toolbar-title>
+      <router-link
+        class="text-decoration-none white--text"
+        :to="{ name: 'administrator.home' }"
+      >
+        {{ appTitle }}
+      </router-link>
+    </v-toolbar-title>
 
     <v-spacer />
 
     <div v-if="$auth.user !== null">
-      @{{ $auth.user.username }}
+      <v-icon>mdi mdi-account</v-icon>
+      <span class="ml-1">{{ $auth.user.username }}</span>
     </div>
   </v-app-bar>
 </template>
