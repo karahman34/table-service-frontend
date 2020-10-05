776b3c75ce10d2b69f5b01a8ac96df360f51a322
karahman <jakarahman007@gmail.com>
2020-10-05 21:35:41 +0700

    change grid value

--- src/components/food/CartDialog.vue
+++ src/components/food/CartDialog.vue
@@ -21,11 +21,17 @@
       <!-- Content -->
       <v-card-text>
         <v-row>
-          <v-col cols="6">
+          <v-col
+            cols="12"
+            sm="6"
+          >
             <v-img :src="food.image" />
           </v-col>
 
-          <v-col cols="6">
+          <v-col
+            cols="12"
+            sm="6"
+          >
             <!-- Alert Message -->
             <v-alert
               v-if="!itemAvailable && !isEdit"
